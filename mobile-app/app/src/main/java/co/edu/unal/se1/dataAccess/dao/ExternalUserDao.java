89ec535f4578e4f9be05de9101c35c42d02ed056
hadiazo <hadiazo@unal.edu.co>
2019-11-04 19:27:19 -0500

    Ícono personalizado para la app

--- mobile-app/app/src/main/java/co/edu/unal/se1/dataAccess/dao/ExternalUserDao.java
+++ mobile-app/app/src/main/java/co/edu/unal/se1/dataAccess/dao/ExternalUserDao.java
@@ -1,14 +1,14 @@
 package co.edu.unal.se1.dataAccess.dao;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
-
 import java.util.List;
-
 import co.edu.unal.se1.dataAccess.model.ExternalUser;
 
+@Dao
 public interface ExternalUserDao {
 
     @Query("SELECT * FROM externaluser")
