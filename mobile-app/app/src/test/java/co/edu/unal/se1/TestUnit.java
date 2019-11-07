9c406c1241518aba5790c0e232a7f22013b6c58f
hadiazo <hadiazo@unal.edu.co>
2019-11-06 23:27:52 -0500

    Métodos para las pruebas de unidad local

--- mobile-app/app/src/test/java/co/edu/unal/se1/TestUnit.java
+++ mobile-app/app/src/test/java/co/edu/unal/se1/TestUnit.java
@@ -1,11 +1,12 @@
 package co.edu.unal.se1;
 
 import android.content.Context;
-import org.junit.*;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import co.edu.unal.se1.businessLogic.controller.*;
+import co.edu.unal.se1.dataAccess.model.ApplicationAccount;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestUnit extends junit.framework.TestCase {
@@ -15,7 +16,14 @@ public class TestUnit extends junit.framework.TestCase {
     Context mockContext;
 
     @Test
-    public void createAppAccountValidator () {
+    public void verifyLoginValidate (ApplicationAccount appAcc, int pass) {
         ApplicationAccountController appAccountController = new ApplicationAccountController(mockContext);
+        assertEquals(appAccountController.verifyLogin(appAcc,pass),true);
+    }
+
+    @Test
+    public void createSavingsAcountValidate (int sourceId, int targetId, double value) {
+        SavingsAccountController sac = new SavingsAccountController(mockContext);
+        assertEquals(sac.sendMoney(sourceId, targetId, value, mockContext),true);
     }
 }
